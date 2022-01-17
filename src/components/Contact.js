60337dbbd7ee7aec9088217e8357db667f438bf7
maesangaline <marciemae@live.com>
2022-01-17 15:59:17 -0500

    Social tags added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,28 +6,20 @@ import { SocialIcon } from "react-social-icons";
 export default function Contact() {
     return (
         <section id="contact" className="relative">
-            <p>I am looking for job opportunities and would love to connect!</p>
-            <div className="social-icons">
-                <a 
-                href="https://github.com/maesangaline/">
-                    <img 
-                    src="./github-icon.svg" 
-                    alt="my github profile" />
-                </a>
-                <SocialIcon
-                url="https://github.com/maesangaline/"
-                network="github"
-                style={{ height: 50, width: 50 }} />
-                <SocialIcon 
-                url="https://www.linkedin.com/in/maesangaline"
-                network="linkedin"
-                style={{ height: 50, width: 50 }} />
-                <a 
-                href="https://www.linkedin.com/in/maesangaline">
-                        <img 
-                        src="./linkedin-icon.svg " 
-                        alt="my linkedin profile" />
-                </a>
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <p>I am looking for job opportunities and would love to connect!</p>
+                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                        <SocialIcon
+                        url="https://github.com/maesangaline/"
+                        network="github"
+                        style={{ height: 100, width: 100 }}
+                        alt="my github" />
+                        <SocialIcon 
+                        url="https://www.linkedin.com/in/maesangaline"
+                        network="linkedin"
+                        style={{ height: 100, width: 100 }} 
+                        alt="my linkedin"/>
+                   </div>
             </div>
         </section>
     )
