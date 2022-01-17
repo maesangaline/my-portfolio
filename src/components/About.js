a6ea0a61efed4790284cc3fd1ea46540b8e634b4
maesangaline <marciemae@live.com>
2022-01-17 17:22:26 -0500

    About update

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                        Hi, I'm Mae.
-                       <br className="hidden lg:inline-block" />Web Designer &amp; Front-end Developer
+                       <br className="hidden lg:inline-block" />A Web Designer &amp; Front-end Developer
                        </h1> 
                        <p className="mb-8 leading-relaxed">
                        I love to create and maintain websites.
